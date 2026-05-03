a58d9078bd6790acc04b939f9a4c6ae2631a8794
sonu mandal <sonu.bcastudent@gmail.com>
2026-05-03 23:44:06 +0530

    Booking model add

---
+++ backend/src/models/booking.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    home: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Home',
+        required: true
+    },
+
+    checkInDate: {
+        type: Date,
+        required: true
+    },
+
+    checkOutDate: {
+        type: Date,
+        required: true
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    
+    guestCount: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'cancelled'],
+        default: 'pending'
+    }
+
+}, 
+{ timestamps: true });
+
+module.exports =mongoose.model("Booking",bookingSchema);
