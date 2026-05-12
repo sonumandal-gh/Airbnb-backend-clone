531c4a1331a8b0c2fcc87de146d71dd461a74295
sonu mandal <sonu.bcastudent@gmail.com>
2026-05-12 09:41:49 +0530

    Update auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -11,7 +11,7 @@ const authMiddleware = async (req, res, next) => {
       req.headers.authorization.startsWith("Bearer")
     ) {
       token = req.headers.authorization.split(" ")[1];
-    } 
+    }
     //Extract token from cookies
     else if (req.cookies?.accessToken) {
       token = req.cookies.accessToken;
